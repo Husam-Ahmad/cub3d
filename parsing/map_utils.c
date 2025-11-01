3b22fe6309cde3a29d88129a3875d614f18c6131
Jozef Pluta <jpluta@c3r3s6.42prague.com>
2025-11-01 16:40:29 +0100

    some more debug, error handling and normes

--- parsing/map_utils.c
+++ parsing/map_utils.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   map_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huahmad <huahmad@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jpluta <jpluta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 13:27:46 by jpluta            #+#    #+#             */
-/*   Updated: 2025/09/23 19:18:13 by huahmad          ###   ########.fr       */
+/*   Updated: 2025/11/01 16:38:36 by jpluta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int find_last_lane(char **map);
+int	find_last_lane(char **map);
 
 int	find_map(char *line)
 {
 	while (*line && *line != '0' && *line != '1')
-			line++;
+		line++;
 	if (*line == '0' || *line == '1')
 		return (1);
 	else
@@ -30,24 +30,20 @@ void	parse_map(char *line, t_data *data)
 
 	if (data->map == NULL)
 	{
-		data->map = (char **)malloc((data->valid_file_data.map_rows + 1) * sizeof(char *));
+		data->map = (char **)malloc((data->valid_file_data.map_rows + 1)
+				* sizeof(char *));
 		if (!data->map)
-		{
-			// free previous allocated memory
 			exit(1);
-		}
-		ft_bzero(data->map, (data->valid_file_data.map_rows + 1) * sizeof(char *));
+		ft_bzero(data->map, (data->valid_file_data.map_rows + 1)
+			* sizeof(char *));
 	}
 	temp = ft_strdup(line);
 	if (!temp)
-	{
-		// free previous allocated memory
 		exit(1);
-	}
 	data->map[find_last_lane(data->map)] = temp;
 }
 
-int find_last_lane(char **map)
+int	find_last_lane(char **map)
 {
 	int	i;
 
